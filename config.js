acd7108466b7dac9245df448ac8925d9c7208510
PC052 <116803350+PC052@users.noreply.github.com>
2022-11-12 08:14:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['62895606423692']
+global.ownernomer = "62895606423692"
+global.premium = ['62895606423692']
 global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.author = 'X-BOT'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -65,9 +65,9 @@ global.myytv = ''
 global.mygc = ""
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'SAD BOT'
-global.akulaku = 'Bot By Papah'
-global.ytname = 'YT PAPAH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'X-BOT'
+global.akulaku = 'Bot By Dapa'
+global.ytname = 'YT Dapaa XD' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
